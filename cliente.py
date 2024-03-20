17fc2f6f4ac1713f7311731650e1183b2c43c053
Ghedyvan <gvbs@ic.ufal.br>
2024-03-20 20:12:08 -0300

    att

--- cliente.py
+++ cliente.py
@@ -1,12 +1,12 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 5000))
+client_socket.connect(('localhost', 8400))
 
 while True:
-  mensagem = input('User 1:')
+  mensagem = input('Usuário: ')
   client_socket.send(mensagem.encode())
   data = client_socket.recv(1024)
-  print(f"User 2: {data.decode()}")
+  print(f"Servidor: {data.decode()}")
 
 client_socket.close()
--- servidor.py
+++ servidor.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 5000))
+server_socket.bind(('localhost', 8400))
 server_socket.listen()
 
 # clientes = []
@@ -10,8 +10,8 @@ server_socket.listen()
 def handle_client(client_socket):
   while True:
     data = client_socket.recv(1024)
-    print(f"User 1: {data.decode()}")
-    mensagem = input('User 2:')
+    print(f"Usuário: {data.decode()}")
+    mensagem = input('Servidor: ')
     client_socket.send(mensagem.encode())
 
 while True:
@@ -20,6 +20,4 @@ while True:
   #clientes.append(client_socket)
   threading.Thread(target=handle_client, args=(client_socket,)).start()
 
-#for cliente in clientes:
-#  cliente.close()
-#server_socket.close()
+server_socket.close()
