6b4138bd252f4793656dc8cc24ee90dde69ff6ec
Ghedyvan <gvbs@ic.ufal.br>
2024-03-20 11:57:09 -0300

    codigo

--- cliente.py
+++ cliente.py
@@ -1,18 +1,12 @@
 import socket
 
-# Cria o socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 8000))
+client_socket.connect(('localhost', 5000))
 
-# Loop para enviar e receber mensagens
 while True:
-  # Envia a mensagem
-  mensagem = input('Cliente:')
+  mensagem = input('User 1:')
   client_socket.send(mensagem.encode())
-  
-  # Recebe a resposta
   data = client_socket.recv(1024)
-  print(f"Servidor: {data.decode()}")
+  print(f"User 2: {data.decode()}")
 
-# Fecha o socket
 client_socket.close()
