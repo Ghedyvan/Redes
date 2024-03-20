edae68bce3f6bc48f39eab2372f4f13dd10e788c
Ghedyvan <gvbs@ic.ufal.br>
2024-03-20 17:06:45 -0300

    att

--- servidor.py
+++ servidor.py
@@ -3,23 +3,23 @@ import threading
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 5000))
-server_socket.listen(1)
+server_socket.listen()
 
-clientes = []
+# clientes = []
 
 def handle_client(client_socket):
   while True:
     data = client_socket.recv(1024)
-    print(f"User1: {data.decode()}")
+    print(f"User 1: {data.decode()}")
     mensagem = input('User 2:')
     client_socket.send(mensagem.encode())
 
 while True:
 
   client_socket, client_address = server_socket.accept()
-  clientes.append(client_socket)
+  #clientes.append(client_socket)
   threading.Thread(target=handle_client, args=(client_socket,)).start()
 
-for cliente in clientes:
-  cliente.close()
-server_socket.close()
+#for cliente in clientes:
+#  cliente.close()
+#server_socket.close()
