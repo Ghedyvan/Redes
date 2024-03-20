fd8757b35cbb3e5d0fab5afcc9977f56bdb22d11
Ghedyvan <gvbs@ic.ufal.br>
2024-03-20 20:12:30 -0300

    att

--- servidor.py
+++ servidor.py
@@ -5,7 +5,6 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 8400))
 server_socket.listen()
 
-# clientes = []
 
 def handle_client(client_socket):
   while True:
@@ -17,7 +16,6 @@ def handle_client(client_socket):
 while True:
 
   client_socket, client_address = server_socket.accept()
-  #clientes.append(client_socket)
   threading.Thread(target=handle_client, args=(client_socket,)).start()
 
 server_socket.close()
